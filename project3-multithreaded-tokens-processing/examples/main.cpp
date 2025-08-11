ffa14ea328d9bfa396578d0ad59e588b9b94ae82
Anas <anasthegreat986@gmail.com>
2025-08-11 14:10:33 +0000

    Fix thread library issue, add examples

--- project3-multithreaded-tokens-processing/examples/main.cpp
+++ project3-multithreaded-tokens-processing/examples/main.cpp
@@ -1,5 +1,14 @@
-#include<iostream>
+#include"token_processing/llminferenceengine.h"
 
 int main() {
+    LLMInferenceEngine engine(3);
+
+    // Submit tasks to be finished
+    engine.submit({"Hello", "world"});
+    engine.submit({"This", "is", "a", "test"});
+    engine.submit({"Goodbye"});
+    engine.submit({"More", "tokens", "for", "processing"});
+
+    // Wait for destructor to be called
     return 0;
 }
